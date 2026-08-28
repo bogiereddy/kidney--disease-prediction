6c8a10a6e99fcad0594c8f869e7bb596b3dcef49
bogiereddy <bogiereddyobulreddy>
2026-08-28 17:20:04 +0530

    added prediction file

---
+++ src/Cnnclassifier/pipeline/prediction.py
@@ -0,0 +1,24 @@
+import numpy as np
+import tensorflow.keras.models import load_model
+import tensorflow.keras.preprocessing import image
+import os
+
+
+class PredictionPipeline:
+    def __init__(self,filename):
+        self.filename =filename
+        model = load_model(os.path.join("artifacts","training","model.h5"))
+        imagename=self.filename
+        test_image = image.load_img(imagename,target_size=(22,224))
+        test_image =image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image,axis=0)
+        result =np.argmax(model.predict(test_image),axis=1)
+        print(result)
+        if result[0] ==1:
+            prediction = 'Tumor'
+            return [{'image':prediction}]
+        else:
+            prediction = 'Normal'
+            return[{"image":prediction}]
+        
+
